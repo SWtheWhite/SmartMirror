35b2e2abf0caa8d8d2f57e86ea73c2a0665dc451
SW Baek <swthewhite@gmail.com>
2023-02-20 15:43:31 +0900

    Add files via upload

---
+++ js/dayinfo.js
@@ -0,0 +1,11 @@
+function dayinfo(){
+    var thatday = new Date("2023-03-02")
+    var today = new Date(); 
+
+    var diff = today-thatday;
+    var dday = Math.floor(diff / (1000*60*60*24)+1);
+    
+    document.getElementById("dday").innerHTML = dday;
+    document.getElementById("info").innerHTML = "개강";
+}
+dayinfo();
